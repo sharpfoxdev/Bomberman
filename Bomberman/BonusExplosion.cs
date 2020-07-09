720b79b93da70dcd263c161e392881850e6ae404
Maddy <vecernice999@email.cz>
2020-07-10 00:25:42 +0200

    bonus explosion

---
+++ Bomberman/BonusExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    class BonusExplosion : GameObject
+    {
+        public BonusExplosion(Game game) : base(game)
+        {
+
+        }
+        public override void Step()
+        {
+            
+        }
+    }
+}
